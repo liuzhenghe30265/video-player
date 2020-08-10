3dbd03ccf0519dd7accd622507dde5fca85ff64e
liuzhenghe30265 <15901450207@163.com>
2020-08-10 17:59:22 +0800

    feat:vue-video-player

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">vue-video-player</router-link> |
+      <router-link to="/about">ckplayer</router-link>
     </div>
     <router-view/>
   </div>
